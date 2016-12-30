b33e14aaf5f21daca50845ccf8c819f9881732b5
YaBoyTony <csgoknightssup@gmail.com>
2016-12-30 03:53:34 -0600

    Add files via upload

---
+++ example.php
@@ -0,0 +1,72 @@
+<!-- CHAT PART PUT THIS WHERE EVER  -->
+      <div id="" class="container">
+<div class="container">
+  <div style="" class="row">
+    <div class="input-field col s12 m4 l2">
+      <input style="color: white;" value="1" id="first_name2" type="text" class="validate">
+      <label class="active" for="first_name2">Amount</label>
+    </div>
+  </div>
+  <script>
+
+
+<script type="text/javascript" src="chat.js"></script>
+    <script type="text/javascript">
+    
+
+    	// strip tags
+    	name = name.replace(/(<([^>]+)>)/ig,"");
+    	
+    	// display name on page
+    	$("#name-area").html("You are: <span>" + name + "</span>");
+    	
+    	// kick off chat
+        var chat =  new Chat();
+    	$(function() {
+    	
+    		 chat.getState(); 
+    		 
+    		 // watch textarea for key presses
+             $("#sendie").keydown(function(event) {  
+             
+                 var key = event.which;  
+           
+                 //all keys including return.  
+                 if (key >= 33) {
+                   
+                     var maxLength = $(this).attr("maxlength");  
+                     var length = this.value.length;  
+                     
+                     // don't allow new content if length is maxed out
+                     if (length >= maxLength) {  
+                         event.preventDefault();  
+                     }  
+                  }  
+    		 																																																});
+    		 // watch textarea for release of key press
+    		 $('#sendie').keyup(function(e) {	
+    		 					 
+    			  if (e.keyCode == 13) { 
+    			  
+                    var text = $(this).val();
+    				var maxLength = $(this).attr("maxlength");  
+                    var length = text.length; 
+                     
+                    // send 
+                    if (length <= maxLength + 1) { 
+                     
+    			        chat.send(text, name);	
+    			        $(this).val("");
+    			        
+                    } else {
+                    
+    					$(this).val(text.substring(0, maxLength));
+    					
+    				}	
+    				
+    				
+    			  }
+             });
+            
+    	});
+    	</script>
